4f216641de3c9b9b2dce7fe42a2b3a60f81a1fba
Gabriel Tursi <gabrieltursi@gmail.com>
2018-07-12 13:30:59 -0300

    Initial commit

---
+++ example.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+  region     = "${var.region}"
+  version = "~> 1.27"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-44bf9f21"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "gabriel-terraform-example"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "terraform_version" {
+	default = "~> 1.27"
+}
